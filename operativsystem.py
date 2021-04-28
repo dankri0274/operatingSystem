98d96ec2b35df3eddfbf91d48c684783d9da8e85
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-04-28 12:58:07 +0200

    Added the file
    Added the main file

---
+++ operativsystem.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+import termcolor
+import stdiomask
+
+def createUser():
+	wtf = " "
+	name = input("Enter full name: ")
+	name.split()
+	def rootUser():
+
+validCommands = ["
+cmd = input(f"{os.getlogin()}@dannyOS}
